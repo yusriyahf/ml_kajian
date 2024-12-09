69d12713b843ceb2b6b14ea02643bae759ef52b4
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-12-09 22:57:02 +0700

    first commit

---
+++ app/face_detection.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pickle
+import os
+from sklearn.metrics.pairwise import cosine_similarity
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from deepface import DeepFace
+from mtcnn.mtcnn import MTCNN
+from PIL import Image
+from numpy import asarray
+
+model = DeepFace.build_model("Facenet")
+
+def get_embedding(image_path):
+    img = load_img(image_path, target_size=(160, 160))
+    img_array = img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+    
+    embedding = model.model.predict(img_array)
+    return embedding[0]
+
+def save_embedding(name, embedding, file_path="embeddings.pkl"):
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            data = pickle.load(f)
+    else:
+        data = {}
+    
+    data[name] = embedding
+    with open(file_path, "wb") as f:
+        pickle.dump(data, f)
+    print(f"Embedding {name} berhasil disimpan.")
+
+def load_embeddings(file_path="embeddings.pkl"):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Tidak ditemukan file embedding di {file_path}")
+    
+    with open(file_path, "rb") as f:
+        data = pickle.load(f)
+    return data
+
+def compare_embeddings(embedding1, embedding2):
+    embedding1 = embedding1.reshape(1, -1)
+    embedding2 = embedding2.reshape(1, -1)
+    similarity = cosine_similarity(embedding1, embedding2)
+    return similarity[0][0]
+
+
+def extract_face(filename, required_size=(160, 160)):
+	image = Image.open(filename)
+	image = image.convert('RGB')
+	pixels = asarray(image)
+	detector = MTCNN()
+	results = detector.detect_faces(pixels)
+
+	if len(results) == 0:
+		return None
+	x1, y1, width, height = results[0]['box']
+
+	x1, y1 = abs(x1), abs(y1)
+	x2, y2 = x1 + width, y1 + height
+
+	face = pixels[y1:y2, x1:x2]
+
+	image = Image.fromarray(face)
+	image = image.resize(required_size)
+	face_array = asarray(image)
+	return face_array
+
+def register_face(name, image_path):
+    face_array = extract_face(image_path)
+    if face_array is None:
+        return "No face detected"
+    
+    temp_image_path = "temp_face_image.jpg"
+    face_image = Image.fromarray(face_array)
+    face_image.save(temp_image_path)
+    
+    embedding = get_embedding(temp_image_path)
+    save_embedding(name, embedding)
+    # Remove the temporary file
+    os.remove(temp_image_path)
+    
+def recognize_face(image_path, embeddings, threshold=0.5):
+    face_array = extract_face(image_path)
+    if face_array is None:
+        return "No face detected"
+    
+    temp_image_path = "temp_face_image.jpg"
+    face_image = Image.fromarray(face_array)
+    face_image.save(temp_image_path)
+    
+    # Get embedding from the temporary file
+    embedding = get_embedding(temp_image_path)
+    
+    # Remove the temporary file
+    os.remove(temp_image_path)
+    
+    max_similarity = -1
+    recognized_name = None
+    for name, registered_embedding in embeddings.items():
+        similarity = compare_embeddings(embedding, registered_embedding)
+        if similarity > max_similarity:
+            max_similarity = similarity
+            recognized_name = name
+    
+    if max_similarity < threshold:
+        return "Unknown"
+    return recognized_name
---
+++ main.py
@@ -0,0 +1,47 @@
+from app.face_detection import register_face, recognize_face, load_embeddings
+from fastapi import FastAPI, File, UploadFile
+import shutil
+import os
+app = FastAPI()
+
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    # create folder if not exist
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+    # check if the file is an image
+    if file.content_type.split('/')[0] != 'image':
+        return {"predict": "Invalid file type"}
+    # Save the uploaded file to the tmp directory
+    file_location = f"tmp/{file.filename}"
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    # Perform prediction
+    result = recognize_face(file_location, load_embeddings(),0.6)
+    # Delete the file after prediction
+    os.remove(file_location)
+    
+    return {"predict": result}
+
+@app.post("/register-face")
+async def register(file: UploadFile = File(...), name: str = None):
+    # create folder if not exist
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+    # check if the file is an image
+    if file.content_type.split('/')[0] != 'image':
+        return {"predict": "Invalid file type"}
+    # Save the uploaded file to the tmp directory
+    file_location = f"tmp/{file.filename}"
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    # Perform prediction
+    register_face(name, file_location)
+    
+    # Delete the file after prediction
+    os.remove(file_location)
+    
+    return {"message": "Face registered successfully"}
