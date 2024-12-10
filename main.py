6db561324093399ad3454a4693dc6b570d74160c
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-12-10 15:02:46 +0700

    Add gender classification

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from app.face_detection import register_face, recognize_face, load_embeddings
+from app.gender_classification import predict_gender as pred_gender
 from fastapi import FastAPI, File, UploadFile
 import shutil
 import os
@@ -45,3 +46,22 @@ async def register(file: UploadFile = File(...), name: str = None):
     os.remove(file_location)
     
     return {"message": "Face registered successfully"}
+
+@app.post("/predict-gender")
+async def predict_gender(file: UploadFile = File(...)):
+    # create folder if not exist
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+    # check if the file is an image
+    if file.content_type.split('/')[0] != 'image':
+        return {"predict": "Invalid file type"}
+    # Save the uploaded file to the tmp directory
+    file_location = f"tmp/{file.filename}"
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    # Perform prediction
+    result = pred_gender(file_location)
+    # Delete the file after prediction
+    os.remove(file_location)
+    return {"predict": result}
